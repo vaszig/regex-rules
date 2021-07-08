c4bc875c3300784f7e3652430cdeddf932f6c019
lakis <zigras00@gmail.com>
2021-07-08 11:32:41 +0200

    fix forms-change to simple from model form

--- app/forms.py
+++ app/forms.py
@@ -1,28 +1,26 @@
 import re
 
-from django.forms import ModelForm
+from django import forms
 from django.core.exceptions import ValidationError
 
-from api.models import Rule
+
+TYPE_OF_SEARCH_CHOICES = [
+        ('contains', 'Default(contains the pattern)'),
+        ('match_in', 'In(includes a whole word)'),
+        ('match_out', 'Out(excludes a whole word)'),
+    ]
 
 
-class ResultsForm(ModelForm):
+class ResultsForm(forms.Form):
     """Form for validating user's input. Column is the column to filter and pattern the regex pattern."""
 
-    class Meta:
-        model = Rule
-        fields = ['type_of_search', 'column', 'pattern']
+    type_of_search = forms.ChoiceField(choices=TYPE_OF_SEARCH_CHOICES, label='Enter type of search')
+    column = forms.CharField(max_length=50, label='Enter column to apply regex')
+    pattern = forms.CharField(max_length=100, label='Enter regex pattern')
 
-    def validate_pattern(self, value):
+    def clean_pattern(self):
         """Validates that the given pattern is a valid regex."""
         
-        try:
-            re.compile(value)
-        except re.error as e:
-            return e
-        return value
-    
-    def clean_pattern(self):
         regex_pattern = self.cleaned_data['pattern']
         try:
             re.compile(regex_pattern)
